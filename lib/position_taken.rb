3e82bf69ba2e3bb905d0a5dbf3940379c13bcfd6
Austin Hernandez <ahernandez1170@gmail.com>
2020-11-10 23:39:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,4 +2,9 @@ require_relative '../lib/position_taken.rb'
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
